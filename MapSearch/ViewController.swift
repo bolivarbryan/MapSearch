211f7b4de55984a866374994d2bcc775d904b1a3
Bryan <bolivarbryan@gmail.com>
2017-02-07 11:26:25 -0500

    Initial Commit

---
+++ MapSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapSearch
+//
+//  Created by Bryan on 7/02/17.
+//  Copyright © 2017 BolivarBryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
